9707dafe0e57623e65cf904d2b221e164ab0a6da
sangpilyun <ddonz429@gmail.com>
2022-12-15 00:01:24 +0900

    구조변경 및 번역한 문장 DB에 저장

---
+++ src/dto/translate.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class TranslateDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 255)
+  source: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 255)
+  target: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 5000)
+  text: string;
+}
